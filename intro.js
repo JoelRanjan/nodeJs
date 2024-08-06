3eb41f2044a6f0bc1f571fdf7285a4e6784d2ebe
JoelRanjan <joelranjan009@gmail.com>
2024-08-06 14:37:54 +0530

    intro

---
+++ intro.js
@@ -0,0 +1,7 @@
+/**
+ * 1.what is node js??
+ *
+ * --> node js is a runtime environment for javascript
+ * --> it is used to execute js programs outside the browser
+ * --> not a library or framework
+ */
