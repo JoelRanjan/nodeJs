bd5b8f3608f6120c1650f929f2bd67b16622c43d
JoelRanjan <joelranjan009@gmail.com>
2024-08-21 01:50:38 +0530

    usage of ejs

--- short-url/controllers/url.js
+++ short-url/controllers/url.js
@@ -11,8 +11,8 @@ async function handleGenerateNewShortUrl(req, res) {
     redirectUrl: body.url,
     visitHistory: [],
   });
-
-  return res.json({ id: shortID });
+  return res.render("home", { id: shortID });
+  //return res.json({ id: shortID });
 }
 
 module.exports = { handleGenerateNewShortUrl };
--- short-url/index.js
+++ short-url/index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const Url = require("./models/url");
 const urlRoute = require("./routes/url");
+const staticRoute = require("./routes/staticRouter");
 const { connectToMongoDB } = require("./connect");
 
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url")
@@ -9,10 +11,24 @@ connectToMongoDB("mongodb://127.0.0.1:27017/short-url")
   .catch((err) => console.log(err));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/url", urlRoute);
 
-app.get("/:shortId", async (req, res) => {
+app.use("/", staticRoute);
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+app.get("/test", async (req, res) => {
+  const allUrls = await Url.find({});
+  console.log(allUrls);
+  res.render("home", {
+    urls: allUrls,
+  });
+});
+
+app.get("/url/:shortId", async (req, res) => {
   const shortId = req.params.shortId;
   const entry = await Url.findOneAndUpdate(
     {
